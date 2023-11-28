42a710dbcb60d7e7635e7c465e9a64b788428fcf
Alona Chubenko <aliona.chubenko@gmail.com>
2023-11-28 10:53:54 +0200

    Start a project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
